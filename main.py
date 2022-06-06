2fda3ef60063e4bc6d5b09c00e64f442c4a22baf
Herbie Crisman <106975412+cwkherbert57742@users.noreply.github.com>
2022-06-06 15:56:18 +0000

    did mad lib

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Pandas are cute")
+name = input("gimmie a name")
+adjective = input("gimmie an adjective")
+noun = input("gimmie a noun")
+
+
+print("hi my name is "+name)
+print("and I" +adjective)
+print("to" +noun)
